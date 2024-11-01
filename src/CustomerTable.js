bbbf2a12e99a03ed4ea10d4c163ce41b5ede3c1b
abdelrahman <abdelrahmanashour758@gmail.com>
2024-11-01 23:44:31 +0200

    Excel file added

--- src/CustomerTable.js
+++ src/CustomerTable.js
@@ -1,9 +1,11 @@
 import { useState, useContext } from "react";
 import ServicesContext from "./ServicesContext";
+import * as XLSX from "xlsx";
 
 export default function CustomerTable() {
   const { getServicesByCode } = useContext(ServicesContext);
   const [rows, setRows] = useState([]);
+  const [excelTable, setExcelTable] = useState([]);
 
   const addRow = () => {
     setRows([
@@ -23,7 +25,40 @@ export default function CustomerTable() {
     ]);
   };
 
-  const clearTable = () => { setRows([]); };
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const json = XLSX.utils.sheet_to_json(worksheet);
+
+      let arr = [];
+      json.forEach((row) => {
+        const service = getServicesByCode(+row.code)[0];
+        console.log(row);
+        if (row.no && row.interarrival !== null && row.code) {
+          arr.push({
+            no: row.no,
+            interarrival: row.interarrival,
+            arrivalTime: "",
+            code: row.code,
+            title: service.name,
+            begin: "",
+            duration: service.time,
+            end: "",
+            systemState: "",
+            customerState: "",
+          });
+        }
+      });
+
+      setRows([...arr]);
+    };
+    reader.readAsArrayBuffer(file);
+  };
 
   const handleInputChange = (index, field, value) => {
     const newRows = [...rows];
@@ -71,91 +106,111 @@ export default function CustomerTable() {
     setRows(newRows);
   };
 
+  const clearTable = () => {
+    setRows([]);
+  };
+
   return (
-    <table aria-hidden="false">
-      <thead>
-        <tr>
-          <th colSpan={3}>Customer</th>
-          <th colSpan={2}>Service</th>
-          <th colSpan={3}>In Service (Time)</th>
-          <th>Customer</th>
-          <th>System</th>
-        </tr>
-        <tr>
-          <th>NO.</th>
-          <th>Interarrival</th>
-          <th>Arrival Time</th>
-          <th>Code</th>
-          <th>Title</th>
-          <th>Begin</th>
-          <th>Duration</th>
-          <th>End</th>
-          <th>State</th>
-          <th>State</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, index) => (
-          <tr key={index}>
-            <td>
-              {" "}
-              <input
-                type="text"
-                value={row.no}
-                onChange={(e) => handleInputChange(index, "no", e.target.value)}
-              />{" "}
-            </td>
-            <td>
-              {" "}
-              <input
-                type="text"
-                value={row.interarrival}
-                onChange={(e) =>
-                  handleInputChange(index, "interarrival", e.target.value)
-                }
-              />{" "}
-            </td>
-            <td>{row.arrivalTime}</td>
-            <td>
-              <input
-                type="number"
-                value={row.code}
-                onChange={(e) =>
-                  handleCodeService(index, "code", e.target.value)
-                }
-              />
-            </td>
-            <td>{row.title}</td>
-            <td>{row.begin}</td>
-            <td>{row.duration}</td>
-            <td>{row.end}</td>
-            <td>{row.customerState}</td>
-            <td>{row.systemState}</td>
+    <>
+      <table aria-hidden="false">
+        <thead>
+          <tr>
+            <th colSpan={3}>Customer</th>
+            <th colSpan={2}>Service</th>
+            <th colSpan={3}>In Service (Time)</th>
+            <th>Customer</th>
+            <th>System</th>
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colSpan={2}>
-            <button className="btn" onClick={addRow}>
-              ADD CUSTOMER
-            </button>
-          </td>
-          <td colSpan={2}></td>
-          <td style={{ textAlign: "center" }}>
-            <button className="btn" onClick={recalculateTimes}>
-              SIMULATE
-            </button>
-          </td>
-          <td colSpan={3}></td>
-          <td colSpan={2} style={{ textAlign: "right" }}>
-            {" "}
-            <button className="btn" onClick={clearTable}>
-              CLEAR TABLE
-            </button>{" "}
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+          <tr>
+            <th>NO.</th>
+            <th>Interarrival</th>
+            <th>Arrival Time</th>
+            <th>Code</th>
+            <th>Title</th>
+            <th>Begin</th>
+            <th>Duration</th>
+            <th>End</th>
+            <th>State</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <td>
+                {" "}
+                <input
+                  type="text"
+                  value={row.no}
+                  onChange={(e) =>
+                    handleInputChange(index, "no", e.target.value)
+                  }
+                />{" "}
+              </td>
+              <td>
+                {" "}
+                <input
+                  type="text"
+                  value={row.interarrival}
+                  onChange={(e) =>
+                    handleInputChange(index, "interarrival", e.target.value)
+                  }
+                />{" "}
+              </td>
+              <td>{row.arrivalTime}</td>
+              <td>
+                <input
+                  type="number"
+                  value={row.code}
+                  onChange={(e) =>
+                    handleCodeService(index, "code", e.target.value)
+                  }
+                />
+              </td>
+              <td>{row.title}</td>
+              <td>{row.begin}</td>
+              <td>{row.duration}</td>
+              <td>{row.end}</td>
+              <td>{row.customerState}</td>
+              <td>{row.systemState}</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={2}>
+              <button className="btn" onClick={addRow}>
+                ADD CUSTOMER
+              </button>
+            </td>
+            <td colSpan={4}></td>
+            <td colSpan={4} className="excel-container">
+              Upload Excel file:
+              <button className="btn">
+                <input
+                  type="file"
+                  accept=".xlsx, .xls"
+                  onChange={handleFileUpload}
+                />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="btn" onClick={recalculateTimes}>
+                SIMULATE
+              </button>
+            </td>
+            <td colSpan={7}></td>
+            <td colSpan={2}>
+              {" "}
+              <button className="btn" onClick={clearTable}>
+                CLEAR TABLE
+              </button>{" "}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
   );
 }
